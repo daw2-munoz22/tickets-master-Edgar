36b29c25445eb0e8be92df4c209faa5b0fa9ba29
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2023-05-23 19:44:48 +0200

    Delete src directory

--- src/bd/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-// conexion con supabase
-const supabaseUrl = 'https://xowhbsxzfirkcorazzqb.supabase.co'
-// const supabaseKey = process.env.SUPABASE_KEY
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhvd2hic3h6Zmlya2NvcmF6enFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ4NTY5NTksImV4cCI6MjAwMDQzMjk1OX0.lAkXGx91tdGIV_z9TctKEHVi8a4et8NzfC7aqmcxBEo'
-// exportamos la conexión a supabase
-export const supabase = createClient(supabaseUrl, supabaseKey)
-console.log('Conexion a supabase', supabase)
--- src/vistas/home.js
+++
@@ -1,9 +0,0 @@
-//import { panel } from "./panel";
-export const home =  {
-  template: `
-  <div class="pt-5">
-    <h1 class="w-100 text-center">Gestión de incidencias</h1>
-    
-  </div>
-  `
-}
